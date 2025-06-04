a9dbf24c8d29ac236e40c11dede7503f5d416c0d
Kavya M <kavya.m@s.amity.edu>
2025-06-04 15:57:38 +0530

    Added Book Inventory Functions

--- lib_ms.cpp
+++ lib_ms.cpp
@@ -67,3 +67,102 @@ public:
     }
 };
 
+
+// ========================= Book Inventory Functions =========================
+
+vector<Book> bookList;
+
+// Save books to file
+void saveBooksToFile() {
+    ofstream fout("books.txt");
+    for (const auto& book : bookList) {
+        fout << book.bookID << "," << book.title << "," << book.author << ","
+             << book.genre << "," << book.stock << endl;
+    }
+    fout.close();
+}
+
+// Load books from file
+void loadBooksFromFile() {
+    bookList.clear();
+    ifstream fin("books.txt");
+    string line;
+    while (getline(fin, line)) {
+        int id, stock;
+        string title, author, genre;
+
+        size_t pos = 0;
+        vector<string> tokens;
+        while ((pos = line.find(',')) != string::npos) {
+            tokens.push_back(line.substr(0, pos));
+            line.erase(0, pos + 1);
+        }
+        tokens.push_back(line); // last field
+
+        if (tokens.size() == 5) {
+            id = stoi(tokens[0]);
+            title = tokens[1];
+            author = tokens[2];
+            genre = tokens[3];
+            stock = stoi(tokens[4]);
+            bookList.emplace_back(id, title, author, genre, stock);
+        }
+    }
+    fin.close();
+}
+
+// Add a new book
+void addBook() {
+    int id, stock;
+    string title, author, genre;
+
+    cout << "Enter Book ID: ";
+    cin >> id;
+    cin.ignore();
+    cout << "Enter Title: ";
+    getline(cin, title);
+    cout << "Enter Author: ";
+    getline(cin, author);
+    cout << "Enter Genre: ";
+    getline(cin, genre);
+    cout << "Enter Stock: ";
+    cin >> stock;
+
+    bookList.emplace_back(id, title, author, genre, stock);
+    cout << "Book added successfully!\n";
+    saveBooksToFile();
+}
+
+// Display all books
+void displayAllBooks() {
+    cout << "\nAll Books in Library:\n";
+    cout << "ID   | Title                | Author         | Genre     | Stock\n";
+    cout << "---------------------------------------------------------------\n";
+    for (const auto& book : bookList) {
+        book.display();
+    }
+}
+
+// Search books by title
+void searchBookByTitle() {
+    string query;
+    cout << "Enter book title to search: ";
+    cin.ignore();
+    getline(cin, query);
+    bool found = false;
+
+    for (const auto& book : bookList) {
+        if (book.title.find(query) != string::npos) {
+            if (!found) {
+                cout << "Books matching \"" << query << "\":\n";
+                cout << "ID   | Title                | Author         | Genre     | Stock\n";
+                cout << "---------------------------------------------------------------\n";
+            }
+            book.display();
+            found = true;
+        }
+    }
+    if (!found)
+        cout << "No books found with title containing \"" << query << "\".\n";
+}
+
